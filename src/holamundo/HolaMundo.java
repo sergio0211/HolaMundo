4d72882f8aff228e149723a4fd87da8bc4051cdf
Estudiante <Estudiante@BE401S103C10ING.unal.loc>
2019-04-03 08:38:15 -0500

    Primer commit 2.0

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo, que hace");
     }
     
 }
